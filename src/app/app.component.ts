033fceeffb09096175433e0ac13b14b6dabb7226
Prem <chris.prem11@gmail.com>
2019-04-03 21:47:16 +0530

    made relevant changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,42 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'myApp';
+
+  favourites: any [] = [];
+  data: any[] = [
+    {
+      'id' : 10,
+      'name' : 'Data22',
+      'content' : 'jhjhfkhdkjfhdjfdkjfhkdj',
+      'isFav' : false
+    },
+    {
+      'id' : 11,
+      'name' : 'Data33',
+      'content' : 'poppobjhghgjgjhgjgjhgjjgj',
+      'isFav' : false
+    },
+    {
+      'id' : 12,
+      'name' : 'Data44',
+      'content' : 'kjhjhjhjhghhjhjghjgjhjh',
+      'isFav' : false
+    }
+];
+
+  addToFav(item){
+  const index = this.data.indexOf(item);
+  this.data[index].isFav = true;
+  this.favourites.push(item);
+  console.log(this.favourites);
+  }
+
+  removeFromFav(item){
+  const index = this.data.indexOf(item);
+  this.data[index].isFav = false;
+  this.favourites.splice(index, 1);
+  console.log(this.favourites);
+
+  }
+
 }
